fbe355a2e5587c6d5ebd6c6a710616b01e3d02fa
juan hurtado <juanhurtado4@outlook.com>
2017-09-17 13:49:29 -0700

    roulette app template

---
+++ roulette.py
@@ -0,0 +1,11 @@
+import random
+
+
+bank_account = 1000
+bet_amount = 0
+bet_color = None
+bet_number = None
+
+green = [0, 37]
+red = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36]
+black = [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35]

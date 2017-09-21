bd67c8149b08661aeadc09483c41daefa258b11f
juan hurtado <juanhurtado@juans-MacBook-Pro.local>
2017-09-20 18:46:10 -0700

    Refactor Take_bet class into Player

---
+++ roulette/check_roulette.py
@@ -0,0 +1,19 @@
+nums_colors = {
+                'green':[0, 37],
+                'red':[1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36],
+                'black':[2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35]
+                }
+
+# def check_bet_num(num):
+#     '''
+#     Checks whether or not a number bet was placed.
+#     '''
+#     if num == None:
+#         return '''"{name} placed a bet of ${bet_amount} on the color {bet_color}".format(name=self.name,
+#                                                                                        bet_amount=self.bet_amount,
+#                                                                                        bet_color=self.bet_color)'''
+#     else:
+#         return '''"{name} placed a bet of ${bet_amount} on number {bet_num} and color {bet_color}".format(name=self.name,
+#                                                                                                         bet_amount=self.bet_amount,
+#                                                                                                         bet_num=self.bet_num,
+#                                                                                                         bet_color=self.bet_color)'''

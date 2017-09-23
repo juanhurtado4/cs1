6a054840e5bbb826e6faac9492e00cce00554be5
juan hurtado <juanhurtado4@outlook.com>
2017-09-23 01:50:24 -0700

    Finished index_correct_letter & is_word_guessed

--- hangman/check_hangman.py
+++ hangman/check_hangman.py
@@ -19,65 +19,86 @@ message = {'congrats': 'Congratulations you won the game!',
 alphabet = string.ascii_lowercase
 
 def load_word():
-   # f = open('hangman_words.txt', 'r')
-   # words_list = f.read_lines()
-   # f.close()
-   #
-   # words_list = words_list[0].split(' ')
-   # secret_word = random.choice(words_list)
-   # # return secret_word
-   return 'oxygen'
+   f = open('hangman_words.txt')
+   words_list = f.read()
+   f.close()
+
+   words_list = words_list.split(' ')
+   secret_word = random.choice(words_list)
+   return secret_word
 
 def game_so_far(secret_word, letters_guessed):
     '''
-    Returns the state of the game so far.
+    Receives a string of secret_word and a list of letters_guessed
+    Returns the state of the game so far in a string.
     Underscores for every word that has not been guessed.
     Loop places underscores in place of letter to hide secret word.
     '''
-    if len(letters_guessed) == 0:
-        current_game = ' '.join(['_' for underscore in range(len(secret_word))])
+    current_game = ''.join(['_' for underscore in range(len(secret_word))])
+    if letters_guessed == 0:
+        return current_game
+    else:
+        for letter in letters_guessed:
+            if index_correct_letter(secret_word, letter) != 'false':
+                if type(index_correct_letter(secret_word, letter)) == type(list('test')):
+                    for index in index_correct_letter(secret_word, letter):
+                        current_game = [letter for letter in current_game]
+                        current_game[index_correct_letter(secret_word,letter)[index]] =    letter
+
+                current_game = [letter for letter in current_game]
+                current_game[index_correct_letter(secret_word, letter)] = letter
+            else:
+                continue
+        current_game = ''.join([letter for letter in current_game])
+        return current_game
 
 
 
 
-def is_word_guessed(secret_word, letters_guessed, guesses_left):
+
+
+
+def index_correct_letter(secret_word, letter_guessed):
+    '''
+    Function checks if letter_guessed is correct.
+    If so it returns the index (Int) of the correct letter in the secret word.
+    elif returns list of indexes if a letter is repeated.
+    Else returns false.
+    '''
+    if multiple_lttr_secret_word(secret_word) != 'false':
+        multiple_letters = multiple_lttr_secret_word(secret_word) # Dictionary of letter/index pairs
+        if letter_guessed in multiple_letters:
+            all_index = multiple_letters[letter_guessed]
+            return all_index
+
+
+    elif letter_guessed in secret_word:
+        for index in range(len(secret_word)):
+            if letter_guessed == secret_word[index]:
+                return index
+    else:
+        return 'false'
+
+def is_word_guessed(secret_word, letters_guessed):
     '''
     secret_word: string, the random word the user is trying to guess.  This is selected on line 9.
     letters_guessed: list of letters that have been guessed so far.
     returns: boolean, True only if all the letters of secret_word are in letters_guessed;
       False otherwise
     '''
-    if guesses_left > 0:
-        return guesses_left
-    elif secret_word in letters_guessed:
+    if secret_word in letters_guessed:
         return True
     else:
         return False
 
-
-
-
-def get_guessed_word(secret_word, letters_guessed):
+def multiple_lttr_secret_word(secret_word):
     '''
-    secret_word: string, the random word the user is trying to guess.  This is selected on line 9.
-    letters_guessed: list of letters that have been guessed so far.
-    returns: string, of letters and underscores.  For letters in the word that the user has
-    guessed correctly, the string should contain the letter at the correct position.  For letters
-    in the word that the user has not yet guessed, shown an _ (underscore) instead.
+    USE COUNT FUNCTION.
+    Keep track of all the repeated letters and indexes.
+    If multiple letters returns dictionary letter/index pairs.
+    Else returns false (string).
     '''
-    # FILL IN YOUR CODE HERE...
-
-
-
-
-def getAvailableLetters(letters_guessed):
-    '''
-    letters_guessed: list of letters that have been guessed so far
-    returns: string, comprised of letters that represents what letters have not
-      yet been guessed.
-    '''
-
-
+    return {'l': [2, 3]}
 
 def hangman(secret_word):
     '''
@@ -98,7 +119,10 @@ def hangman(secret_word):
       user has not yet guessed.
     '''
     # FILL IN YOUR CODE HERE...
+    pass
 
-
-secret_word = load_word()
-# hangman(load_word())
+# secret_word = load_word()
+# print(secret_word)
+# print(len(secret_word))
+print(game_so_far('whitehouse', ['e']))
+# print(index_correct_letter('hello', 'h'))

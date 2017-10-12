3a450de1e564c7aae96ccd84159bc8c62537d09c
juan hurtado <juanhurtado4@outlook.com>
2017-10-12 07:32:43 -0700

    Refactored highest in feb func

--- read_write/modified_sales_data.py
+++ read_write/modified_sales_data.py
@@ -1,18 +1,14 @@
 with open('sales_data.txt') as f:
     sales_data = list(f.readlines())
 
+# -------------------MANUAL TESTING ------------------
 testing_sales = [sales_data[0], sales_data[1], sales_data[2]]
 print(testing_sales[0].split('$'))
+print(testing_sales[0].split('\t'))
 money = testing_sales[0].split('$')[1]
 print(money)
-# for index, test in enumerate(sales_data[0]):
-#     # print('char:', test)
-#     # print('ordinal char:', ord(test))
-#     print('test', chr(9))
-#     # print(test[0:test.index(str(ord(9)))])
-#     # print('index:', index)
-#     # print(sales_data[3].find(' '))
-#     # print(sales_data[3].index(' '))
+# -----------------------------------------------------
+
 
 # Question 1
 def sales_transaction_number(sales_data):
@@ -30,11 +26,22 @@ def highest_in_feb(sales_data):
             if sales[slash_index -1] == '2':
                 feb_entries.append(sales)
 
-    for entry in feb_entries:
-        index_dollar = sale.index('$')
-        sale_amount = float(entry[index_dollar + 1:-1])
-        city_last_index = city.index(chr(9)) # First tab in the entry
-        cities[city[0:city_last_index]] += sale_amount
+# def highest_in_feb(sales_data):
+#     feb_entries = []
+#     cities = {}
+#     for sales in sales_data:
+#         slash_index = sale.index('/')
+#         if sales[slash_index - 2] == '1':
+#             continue
+#         else:
+#             if sales[slash_index -1] == '2':
+#                 feb_entries.append(sales)
+#
+#     for entry in feb_entries:
+#         index_dollar = sale.index('$')
+#         sale_amount = float(entry[index_dollar + 1:-1])
+#         city_last_index = city.index(chr(9)) # First tab in the entry
+#         cities[city[0:city_last_index]] += sale_amount
 
 
 # Question 3
